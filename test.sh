f64eca297a476dc15f2a4b50ab5c2e2ec6776c66
Sameer Naik <sameer@bitnami.com>
2016-04-19 18:21:58 +0530

    tests: fix python version test

--- test.sh
+++ test.sh
@@ -31,7 +31,7 @@ cleanup_environment
 @test "python installed" {
   container_create default -id
 
-  run container_exec default python -v
+  run container_exec default python --version
   [ "$status" = 0 ]
 }
 
